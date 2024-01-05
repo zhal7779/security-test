d2c406cbe0c09721f4bc0f5d8373d3740778cde8
minyoung <zhal7779@naver.com>
2024-01-05 17:59:58 +0900

    Feat: 미들웨어 설정

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//express 미들웨어 설정
+app.use(express.static("public"));
+
 //루트("/") 패스 지정
 app.get("/", (req, res, next) => {
   res.send("hello world");
